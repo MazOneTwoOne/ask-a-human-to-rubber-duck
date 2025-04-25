7e5577288ddcc4b205f156acaae8c7b1355e7520
MazOneTwoOne <76905544+MazOneTwoOne@users.noreply.github.com>
2025-04-25 15:28:09 +0100

    update to api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,11 @@
 import { createApp } from '../src/app.js';
 
 export default async (req, res) => {
-  const app = await createApp();
-  app(req, res);
+  try {
+    const app = await createApp();
+    app(req, res);
+  } catch (error) {
+    console.error("Error in serverless function:", error);
+    res.status(500).send("Internal Server Error");
+  }
 };
