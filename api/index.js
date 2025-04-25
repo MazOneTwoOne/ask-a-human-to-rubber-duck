cd75368b6a2b601c5e9b086a3fca825c7c82ef44
MazOneTwoOne <76905544+MazOneTwoOne@users.noreply.github.com>
2025-04-25 15:50:49 +0100

    new index & new appVercel

--- api/index.js
+++ api/index.js
@@ -1,11 +1,6 @@
-import { createApp } from '../src/app.js';
+import { createApp } from '../src/appVercel.js';
 
 export default async (req, res) => {
-  try {
-    const app = await createApp();
-    app(req, res);
-  } catch (error) {
-    console.error("Error in serverless function:", error);
-    res.status(500).send("Internal Server Error");
-  }
+  const app = await createApp();
+  app(req, res);
 };
