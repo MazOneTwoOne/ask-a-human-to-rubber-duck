cc3809acea90a47c40dc39e0d6a74d0e6b01f302
Masum Khan <masum.khan@digital.justice.gov.uk>
2025-05-26 15:46:49 +0100

    move imports

--- api/index.js
+++ api/index.js
@@ -1,16 +1,8 @@
-import app from '../src/index.js';
-import fs from 'fs';
-import path from 'path';
-import { fileURLToPath } from 'url';
+import app from '../index.js';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
+console.log('App is:', app);
 
 export default function handler(req, res) {
   app(req, res);
 }
-
-console.log('App is:', app);
-console.log('__dirname:', __dirname);
-console.log('src exists:', fs.existsSync(path.join(__dirname, '../src/index.js')));
