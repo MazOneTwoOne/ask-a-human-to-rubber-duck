78000d170e20c593573da04958b85465a84f20cc
Masum Khan <masum.khan@digital.justice.gov.uk>
2025-05-30 15:14:00 +0100

    can it build just the first page?

--- routes/index.js
+++ routes/index.js
@@ -27,91 +27,91 @@ router.get('/', (req, res) => {
   res.render('main/index');
 });
 
-/* GET request form page */
-router.get('/step-1', (req, res) => {
-  const formData = req.session.formData || {};
-  res.render('main/step_1', {formData});
-});
+// /* GET request form page */
+// router.get('/step-1', (req, res) => {
+//   const formData = req.session.formData || {};
+//   res.render('main/step_1', {formData});
+// });
 
-/* POST request form page (Validation) */
-router.post('/step-1', (req, res) => {
-  const { yourName, typeOfProblem, email, moreDetail } = req.body;
-  const confirmation_id = Math.floor(Math.random() * 1000);
-  let errors = [];
+// /* POST request form page (Validation) */
+// router.post('/step-1', (req, res) => {
+//   const { yourName, typeOfProblem, email, moreDetail } = req.body;
+//   const confirmation_id = Math.floor(Math.random() * 1000);
+//   let errors = [];
 
-  if (!yourName || yourName.trim() === "") {
-    errors.push({ text: "How many times do we have to teach you this lesson, old man!", href: "#your-name" });
-  }
+//   if (!yourName || yourName.trim() === "") {
+//     errors.push({ text: "How many times do we have to teach you this lesson, old man!", href: "#your-name" });
+//   }
 
-  if (!typeOfProblem) {
-    errors.push({ text: 'What was the reason for the "rubber duck" request?', href: "#typeOfProblem" });
-  }
+//   if (!typeOfProblem) {
+//     errors.push({ text: 'What was the reason for the "rubber duck" request?', href: "#typeOfProblem" });
+//   }
 
-  if (!email || email.trim() === "") {
-    errors.push({ text: "This isn't an email dude", href: "#email" });
-  }
+//   if (!email || email.trim() === "") {
+//     errors.push({ text: "This isn't an email dude", href: "#email" });
+//   }
 
 
-  // If there are errors, re-render form with errors
-  if (errors.length > 0) {
-    req.session.formData = { yourName, typeOfProblem, email, moreDetail };
-    return res.render('main/step_1', { errors, yourName, typeOfProblem, email });
-  }
+//   // If there are errors, re-render form with errors
+//   if (errors.length > 0) {
+//     req.session.formData = { yourName, typeOfProblem, email, moreDetail };
+//     return res.render('main/step_1', { errors, yourName, typeOfProblem, email });
+//   }
 
-  req.session.formData = { yourName, typeOfProblem, problemDescription: typeOfProblemObject[typeOfProblem], email, moreDetail, confirmation_id };
+//   req.session.formData = { yourName, typeOfProblem, problemDescription: typeOfProblemObject[typeOfProblem], email, moreDetail, confirmation_id };
 
-  res.redirect('/check-answers');
-});
+//   res.redirect('/check-answers');
+// });
 
-/* GET check answers page. */
-router.get('/check-answers', (req, res) => {
-  const formData = req.session.formData;
+// /* GET check answers page. */
+// router.get('/check-answers', (req, res) => {
+//   const formData = req.session.formData;
 
-  res.render('main/check_answers', {formData});
-});
+//   res.render('main/check_answers', {formData});
+// });
 
-/* GET step 3 page. */
-router.get('/step-3', (req, res) => {
-  res.render('main/step_3');
-});
+// /* GET step 3 page. */
+// router.get('/step-3', (req, res) => {
+//   res.render('main/step_3');
+// });
 
-/* GET notify page. */
-router.get('/notify', async (req, res) => {
-  const formData = req.session.formData;
-  const templateId = process.env.NOTIFY_TEMPLATE_ID;
+// /* GET notify page. */
+// router.get('/notify', async (req, res) => {
+//   const formData = req.session.formData;
+//   const templateId = process.env.NOTIFY_TEMPLATE_ID;
 
-  let jwtToken;
+//   let jwtToken;
 
-  // Make the API call using JWToken
-  try {
-    const jwtToken = await createJwtToken();
-    const response = await req.axiosMiddleware.post(
-      'https://api.notifications.service.gov.uk/v2/notifications/email',
-      {
-        email_address: formData.email,
-        template_id: templateId,
-        personalisation: { 
-          name: formData.yourName, 
-          typeOfProblem: formData.problemDescription, 
-          moreDetail: formData.moreDetail || "No additional information entered"
-        }, 
-      },
-      {
-        headers: {
-          Authorization: `Bearer ${jwtToken}`,
-          'Content-type': 'application/json',
-        },
-      }
-    );
+//   // Make the API call using JWToken
+//   try {
+//     const jwtToken = await createJwtToken();
+//     const response = await req.axiosMiddleware.post(
+//       'https://api.notifications.service.gov.uk/v2/notifications/email',
+//       {
+//         email_address: formData.email,
+//         template_id: templateId,
+//         personalisation: { 
+//           name: formData.yourName, 
+//           typeOfProblem: formData.problemDescription, 
+//           moreDetail: formData.moreDetail || "No additional information entered"
+//         }, 
+//       },
+//       {
+//         headers: {
+//           Authorization: `Bearer ${jwtToken}`,
+//           'Content-type': 'application/json',
+//         },
+//       }
+//     );
 
-    console.log('Email sent:', response.data);
-    res.render('main/notify', { formData, message: 'Successfully sent e-mail.' });
-  } catch (error) {
-    console.log(`token: '${jwtToken}'`)
-    console.error('Error sending email:', error.response ? error.response.data : error.message);
-    res.render('main/notify', { formData, message: 'Failed to send e-mail.' });
-  }
-});
+//     console.log('Email sent:', response.data);
+//     res.render('main/notify', { formData, message: 'Successfully sent e-mail.' });
+//   } catch (error) {
+//     console.log(`token: '${jwtToken}'`)
+//     console.error('Error sending email:', error.response ? error.response.data : error.message);
+//     res.render('main/notify', { formData, message: 'Failed to send e-mail.' });
+//   }
+// });
 
 
 export default router;
